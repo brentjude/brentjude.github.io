a32e3cfab2e485708a53ce407f269a5c48c1cc94
brentjude <128562270+brentjude@users.noreply.github.com>
2023-03-22 10:23:33 +0800

    Adding website to this new repository

---
+++ js/script.js
@@ -0,0 +1,46 @@
+// init Isotope
+var $container = $('#container').isotope({
+    itemSelector: '.grid-item',
+    percentPosition: true,
+    masonry: {
+      columnWidth: '.grid-sizer',
+      gutter: 55
+    }
+});
+
+// layout Isotope after each image loads
+$container.imagesLoaded().progress( function() {
+    $container.isotope('layout');
+  });  
+
+// filter with selects and checkboxes
+var $checkboxes = $('.portfolio--nav input');
+
+$checkboxes.change( function() {
+    // map input values to an array
+    var inclusives = [];
+
+    // inclusive filters from checkboxes
+    $checkboxes.each( function( i, elem ) {
+        // if checkbox, use value if checked
+        if ( elem.checked ) {
+        inclusives.push( elem.value );
+        }
+    });
+
+    // combine inclusive filters
+    var filterValue = inclusives.length ? inclusives.join(', ') : '*';
+
+    $container.isotope({ filter: filterValue })
+});
+
+
+
+//contact form jquery
+$( '.js-input' ).keyup(function() {
+    if( $(this).val() ) {
+       $(this).addClass('not-empty');
+    } else {
+       $(this).removeClass('not-empty');
+    }
+  });
